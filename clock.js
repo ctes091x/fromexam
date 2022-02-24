5b4d04e890ba78143e36025872cda9632121ad2c
CottonTesseract <98365960+ctes091x@users.noreply.github.com>
2022-02-24 09:50:25 +0900

    Update clock.js
    display up to two decimal places

--- clock.js
+++ clock.js
@@ -2,14 +2,12 @@ function getMyDate() {
 	var now = new Date();
 	var exam = new Date(2022,3-1,2,8,40);
 	
-	var examsec = Math.floor((exam - now)/1000);
+	var examsec = Math.round((exam - now) * 100) / 100000;
 	
 	sec.innerHTML = examsec;
-	min.innerHTML = Math.floor(examsec/60);
-	hour.innerHTML = Math.floor(examsec/3600);
 }
 
 var evName = "学年末考査";
 inev.innerHTML = evName;
 
-setInterval(getMyDate,1000);
+setInterval(getMyDate,10);
