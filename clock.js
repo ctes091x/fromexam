03a35fc5c649f718b6c3f3a960e1c34c48c86d4b
CottonTesseract <98365960+ctes091x@users.noreply.github.com>
2022-02-28 12:20:04 +0900

    make code clean

--- clock.js
+++ clock.js
@@ -5,8 +5,6 @@ function getMyDate() {
 	var examsec = Math.floor((exam - now)/1000);
 	
 	sec.innerHTML = examsec;
-	min.innerHTML = Math.floor(examsec/60);
-	hour.innerHTML = Math.floor(examsec/3600);
 }
 
 var evName = "学年末考査";
