c11a99e40c37357a01e222a6e9b5924d433d0705
CottonTesseract <98365960+ctes091x@users.noreply.github.com>
2022-10-03 17:39:57 +0900

    Create patch-for-j3s2

---
+++ clock-patch-for-j3s2.js
@@ -0,0 +1,22 @@
+function getMyDate() {
+	var now = new Date();
+	var exam = new Date(2022,10-1,13,8,40);
+	
+	var examsec = Math.floor((exam - now)/1000);
+	
+	sec.innerHTML = examsec;
+	
+	if ( examsec < 100000 ) {
+		sec.classList.add('warning');
+	}
+	
+	if ( examsec < 0 ) {
+		sec.classList.remove('warning');
+		sec.classList.add('safe');
+	}
+}
+
+var evName = "期末考査";
+inev.innerHTML = evName;
+
+setInterval(getMyDate,1000);
