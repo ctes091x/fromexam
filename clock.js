bb0b36608f19897c432a5d7425045b16f799b8ca
CottonTesseract <98365960+ctes091x@users.noreply.github.com>
2023-01-08 09:47:55 +0900

    Update clock.js for 冬期休暇終了

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 function getMyDate() {
 	var now = new Date();
-	var exam = new Date(2022,12-1,10,10,40);
+	var exam = new Date(2023,1-1,10,8,20);
 	
 	var examsec = Math.floor((exam - now)/1000);
 	
@@ -16,7 +16,7 @@ function getMyDate() {
 	}
 }
 
-var evName = "期末考査終了";
+var evName = "冬期休暇終了";
 inev.innerHTML = evName;
 
 setInterval(getMyDate,1000);
