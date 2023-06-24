61ec60ac5f2b3080af56ef8f9d8da6aaf97c443f
UHAsikakutou <91722200+UHAsikakutou@users.noreply.github.com>
2023-06-24 10:56:53 +0900

    Update clock.js
    週間・日・時間・分・秒表記

--- clock.js
+++ clock.js
@@ -1,10 +1,16 @@
 function getMyDate() {
 	var now = new Date();
 	var exam = new Date(2023,7-1,5,8,40);
-	
-	var examsec = Math.floor((exam - now)/1000);
-	
-	sec.innerHTML = examsec;
+
+	var diff= exam - now
+	var examsec = Math.floor((diff)/1000);
+	var examweek = Math.floor(diff) / (7 * 24 * 60 * 60 * 1000)
+	var examday = Math.floor(diff - (examweek*7*24*60*60*1000)) / (24 * 60 * 60 * 1000)
+	var examhour = Math.floor(diff - (examweek*7*24*60*60*1000) - (examday*24*60*60*1000)) / (60 * 60 * 1000)
+	var exammin = Math.floor(diff - (examweek*7*24*60*60*1000) - (examday*24*60*60*1000) - (examhour*60*60*1000)) / (60 * 1000)
+	var examsec2 = Math.floor(diff - (examweek*7*24*60*60*1000)- (examday*24*60*60*1000) - (examhour*60*60*1000) - (exammin*60*1000)) / 1000	
+	sec.innerHTML = examsec
+	left.innnerHTNL= examweek+"週"+examday+"日"+examhour+"時間"+exammin+"分"+examsec2+"秒"
 	
 	if ( examsec < 100000 ) {
 		sec.classList.add('warning');
