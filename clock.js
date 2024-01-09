6e164df3ea4bbd28521871a2fc0a07c5174dc531
CottonTesseract <98365960+ctes091x@users.noreply.github.com>
2024-01-09 10:35:25 +0900

    Update clock.js for 学年末考査

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 function getMyDate() {
 	var now = new Date();
-	var exam = new Date(2023,12-1,6,8,40);
+	var exam = new Date(2024,3-1,2,8,40);
 	
 	var examsec = Math.floor((exam - now)/1000);
 	
@@ -16,7 +16,7 @@ function getMyDate() {
 	}
 }
 
-var evName = "期末考査";
+var evName = "学年末考査";
 inev.innerHTML = evName;
 
 setInterval(getMyDate,1000);
